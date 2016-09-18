d89bbc04c2ee347dbbf56485ee061fab22fa8d94
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2016-09-18 10:33:58 +0900

    Change shortcut key for gnome-terminal

--- init.el
+++ init.el
@@ -149,7 +149,7 @@
 (global-set-key (kbd "M-P")          'cua-scroll-down)
 
 (global-set-key (kbd "M-m") (lambda () (interactive) (call-interactively 'man)))
-(global-set-key (kbd "C-; C-q") (lambda () (interactive) (shell-command "gnome-terminal")))
+(global-set-key (kbd "C-; C-l") (lambda () (interactive) (shell-command "gnome-terminal")))
 (global-set-key (kbd "C-; C-b") 'browse-url)
 
 (add-hook 'dired-mode-hook
