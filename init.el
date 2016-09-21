34d05f76777fb56a5f1b4ed846f83bf3d7ca1f31
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2016-09-22 01:09:47 +0900

    Use web-mode for .erb files

--- init.el
+++ init.el
@@ -110,6 +110,7 @@
 ;; web-mode: http://web-mode.org/
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (setq web-mode-code-indent-offset 2)
 
 (require 'coffee-mode)
