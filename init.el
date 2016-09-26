154d660ab03bed6f47c13250097e027e51f493e9
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2016-09-27 00:08:00 +0900

    Set indent for web-mode

--- init.el
+++ init.el
@@ -146,6 +146,7 @@
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (setq web-mode-code-indent-offset 2)
+(setq web-mode-markup-indent-offset 2)
 
 (require 'coffee-mode)
 
